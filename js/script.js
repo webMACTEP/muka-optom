cfe88e57ce256c60854696a636c173725f7c93ce
webMACTEP <70036631+webMACTEP@users.noreply.github.com>
2022-12-06 23:04:03 +0300

    fish

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
 jQuery(document).ready(function ($) {
 
+   //swiper slider
+   if ($('.swiper-slide').length > 1) {
+      var swiper = new Swiper('.swiper-container', {
+         navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+         },
+         loop: true,
+         slidesPerView: 3,
+         spaceBetween: 30,
+         pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+         },
+         autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+         },
+         simulateTouch: true,
+      });
+   } else {
+      $('.swiper-button-next').css('display', 'none');
+      $('.swiper-button-prev').css('display', 'none');
+   }
+
 
 });
